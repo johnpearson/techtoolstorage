ec8d4c7e138bbddc6786be95261e398d153db9ee
jpearson <jpearson556@gmail.com>
2011-11-02 11:30:30 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techtoolstorage::Application.config.secret_token = '8a03fa9146a55399e682b0d438aa19348f4345f3612ec783bceaf1333e0f33cfc9438dcbdee2a11d49402eebd21ab3cd7a6d2939b9e1e0109cf77296ec8dce68'
